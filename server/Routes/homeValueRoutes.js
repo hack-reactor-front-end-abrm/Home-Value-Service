2ca73fe1aaecab7c05cb68a9bc8ee377e01f4c82
Raihan <raihanhossain0@gmail.com>
2019-03-28 19:04:13 -0400

    periodical update

---
+++ server/Routes/homeValueRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const homeValueController = require('../controllers/homeValueControllers.js');
+
+router.get('/', homeValueController.)
+
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-//create server file here
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.use('/', homeValueRoutes);
+
+app.listen(3009, function() {
+    console.log('listening on port 3009');
+});
